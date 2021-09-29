b22d67d0da09a08109e3c24e805c4a00af1ebad0
jinkyung <jinkyung010310@gmail.com>
2021-09-29 16:34:28 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [ #서버 IP/blog/
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+ #   path('<int:pk>/', views.single_post_page),
+ #   path('', views.index),
+
+
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 ]
