13e675ba631638545ac96bcece5398bb3ef19555
jinkyung <jinkyung010310@gmail.com>
2021-10-27 12:43:31 +0900

    네비게이션 바와 푸터 모듈화하기

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [ #서버 IP/blog/
  #   path('<int:pk>/', views.single_post_page),
  #   path('', views.index),
 
-
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Post
+
 
 # Create your views here.
+from blog.models import Post
+
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
