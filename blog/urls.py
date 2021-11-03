f2548b9d2fcbaf3057a2af4594ed0795686ae2b1
jinkyung <jinkyung010310@gmail.com>
2021-11-03 17:21:49 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ #서버 IP/blog/
  #   path('<int:pk>/', views.single_post_page),
  #   path('', views.index),
 
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버 IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
