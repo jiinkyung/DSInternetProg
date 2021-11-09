9896e1e3bd28d7961ebf9188030326f6642d2298
jinkyung <jinkyung010310@gmail.com>
2021-11-09 23:23:52 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [ #서버 IP/blog/
  #   path('<int:pk>/', views.single_post_page),
  #   path('', views.index),
 
+
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버 IP/blog/category/slug
