f9aa36d8cb7203325160cc76675ac7945f0d144b
jinkyung <jinkyung010310@gmail.com>
2021-11-14 00:40:38 +0900

    폼으로 포스트 상세 페이지에 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [ #서버 IP/blog/
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버 IP/blog/category/slug
+    path('<int:pk>/new_comment/', views.new_comment),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 
